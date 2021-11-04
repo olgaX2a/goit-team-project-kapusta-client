f0b55b0f79d05b35932ceadb95fa3e430c5b740a
olgaX2a <sekretaryova.olga@gmail.com>
2021-11-05 00:38:39 +0200

    Styles
    Added sass-module, Container component and
    general mixins and variables, fonts.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Container from './components/shared/Container/Container';
 
 function App() {
-  return <div className="App">Start!</div>;
+  return (
+    <Container>
+      <p>Start!</p>
+    </Container>
+  );
+  // return <div className="App">Start!</div>;
 }
 
 export default App;
