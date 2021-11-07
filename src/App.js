49cdfef46343abb743fc5fe3185a2bdf3ce93d74
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-11-07 13:47:40 +0200

    Edited App.js to remove Header component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Container from './components/shared/Container/Container';
-import Header from './components/Header/Header';
 
 function App() {
   return (
     <Container>
-      <Header />
-      {/* <p>Start!</p> */}
+      <p>Start!</p>
     </Container>
   );
   // return <div className="App">Start!</div>;
