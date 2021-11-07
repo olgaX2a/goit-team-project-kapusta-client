224b1ddfaf1f7c3a23eb28fe77bad31d338c0024
KokorovetsTatiana <tanuska@ukr.net>
2021-11-07 18:48:42 +0300

    fixed

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,16 +1,32 @@
+import React, { useState } from 'react';
 import Avatar from 'react-avatar';
 import { ReactComponent as LogOutIcon } from '../../assets/logout.svg';
+import Modal from '../shared/Modal/Modal';
+
 import styles from './UserMenu.module.scss';
 
-function UserMenu() {
+const UserMenu = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = e => {
+    setModalOpen(!isModalOpen);
+  };
+
   return (
     <div className={styles.container}>
       <Avatar name="Test User" unstyled={true} className={styles.avatar} />
       <span className={styles.userNameText}>Test User</span>
-      <button type="button" className={styles.logOutBtn}>
+      <button type="button" className={styles.logOutBtn} onClick={toggleModal}>
         <LogOutIcon className={styles.logOutIcon} />
-        <span className={styles.logOutText}>Выйти</span>
+        <p className={styles.logOutText}>Выйти</p>
       </button>
+      {isModalOpen && (
+        <Modal
+          title={'Вы действительно хотите выйти?'}
+          onNo={toggleModal}
+          onYes={() => console.log('Написать маршрут после разлогинизации')}
+        />
+      )}
     </div>
   );
 }
