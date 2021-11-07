3ff714f0bfa5a37b0d6c3bf4886c606cfa593884
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-11-07 13:45:10 +0200

    Adds final markup and styles for logo and header.
    Also, prepared comments for future editing when auth store and reducer will be ready.

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,18 @@
+// import { NavLink } from 'react-router-dom';
 import { ReactComponent as LogoIcon } from '../../assets/logo.svg';
 import styles from './Logo.module.scss';
 
 function Logo() {
   return (
-    <a href="https://www.google.com/" className={styles.logo} aria-label="Логотип сайта">
+    <a href="#" className={styles.logoLink} aria-label="Логотип сайта">
       <LogoIcon className={styles.logoIcon} />
     </a>
   );
+  // return (
+  //   <NavLink exact to="/" className={styles.logoLink}>
+  //     <LogoIcon className={styles.logoIcon} />
+  //   </NavLink>
+  // );
 }
 
 export default Logo;
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -7,12 +7,40 @@ function UserMenu() {
     <div className={styles.container}>
       <Avatar name="Test User" unstyled={true} className={styles.avatar} />
       <span className={styles.userNameText}>Test User</span>
-      <a type="button" className={styles.logOutBtn}>
+      <button type="button" className={styles.logOutBtn}>
         <LogOutIcon className={styles.logOutIcon} />
         <span className={styles.logOutText}>Выйти</span>
-      </a>
+      </button>
     </div>
   );
 }
 
 export default UserMenu;
+
+//TO BE ADDED (when auth store and reducer is ready):
+
+// import { useDispatch, useSelector } from 'react-redux';
+// import { authSelectors, authOperations } from '../../redux/auth';
+// import Avatar from 'react-avatar';
+// import { ReactComponent as LogOutIcon } from '../../assets/logout.svg';
+// import styles from './UserMenu.module.scss';
+
+// function UserMenu() {
+//   const dispatch = useDispatch();
+//   const name = useSelector(authSelectors.getUsername);
+
+//   return (
+//     <div className={styles.container}>
+//       <Avatar name={name} unstyled={true} className={styles.avatar} />
+//       <span className={styles.userNameText}>{name}</span>
+//       <button
+//         type="button"
+//         onClick={() => dispatch(authOperations.logout())}
+//         className={styles.logOutBtn}
+//       >
+//         <LogOutIcon className={styles.logOutIcon} />
+//         <span className={styles.logOutText}>Выйти</span>
+//       </button>
+//     </div>
+//   );
+// }
