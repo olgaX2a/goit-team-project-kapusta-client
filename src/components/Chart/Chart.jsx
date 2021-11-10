3cfa575cf1d00f8951a12cca3772c63f7a4c9c24
IiIymik <sambotemp@gmail.com>
2021-11-10 18:29:29 +0200

    fixed EsLint issues

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,4 +1,5 @@
-// import { useState, useEffect } from 'react';
+import React from 'react';
+
 import { useMediaQuery } from './useMediaQuery';
 
 import DesktopCharts from './DesktopCharts';
@@ -11,24 +12,22 @@ const Chart = () => {
   const isMatches = useMediaQuery('(min-width: 768px)');
 
   const data1 = [
-    { description: 'Свинина', total: 5000 },
-    { description: 'Говядина', total: 4500 },
-    { description: 'Курица', total: 3200 },
-    { description: 'Рыба', total: 2100 },
-    { description: 'Панини', total: 1800 },
-    { description: 'Кофе', total: 1700 },
-    { description: 'Спагетти', total: 1500 },
-    { description: 'Шоколад', total: 800 },
-    // { description: 'Маслины', total: 500 },
-    // { description: 'Зелень', total: 300 },
+    { description: 'Canna', total: 5000 },
+    { description: 'Robinia', total: 4500 },
+    { description: 'Kappa', total: 3200 },
+    { description: 'PABA', total: 2100 },
+    { description: 'hahn', total: 1800 },
+    { description: 'Kobs', total: 1700 },
+    { description: 'Careen', total: 1500 },
+    { description: 'Wovoka', total: 800 },
   ];
 
   const sortBy = field => (a, b) => a[field] < b[field] ? 1 : -1;
 
   const newData = data1
-    ?.reduce((acc, { description, total }) => {
+    .reduce((acc, { description, total }) => {
       const myCategory = description;
-      const newArr = acc?.find(el => el.description === description);
+      const newArr = acc.find(el => el.description === description);
       if (!newArr) {
         acc.push({ description: myCategory, total });
       }
@@ -41,7 +40,7 @@ const Chart = () => {
     }, [])
     .sort(sortBy('total'));
 
-  const dataChart = newData?.length ? newData : [0];
+  const dataChart = newData.length ? newData : [0];
 
   return isMatches ? (
     <div className={`${styles.container} ${styles.charts}`}>
@@ -55,13 +54,3 @@ const Chart = () => {
 };
 
 export default Chart;
-// {descriprion:"Свинина",total:"5000", },
-// {descriprion:"Говядина",total:"4500",},
-// {descriprion:"Курица",total:"3200",},
-// {descriprion:"Рыба",total:"2100",},
-// {descriprion:"Панини",total:"1800", },
-// {descriprion:"Кофе",total:"1700",},
-// {descriprion:"Спагетти",total:"1500",},
-// {descriprion:"Шоколад",total:"800", },
-// {descriprion:"Маслины",total:"500",},
-// {descriprion:"Зелень",total:"300", },
