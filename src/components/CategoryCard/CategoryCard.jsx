561d93ea0f7c5566d8b8f04c987484a9040fa063
olgaX2a <sekretaryova.olga@gmail.com>
2021-11-14 00:45:19 +0200

    🧱 Interactive component CategoryCard
    Role - button.

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -2,14 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CategoryIcon from './CategoryIcon';
 import styles from './Category.module.scss';
+import expense from '../../utils/expenseTypes';
 
 function CategoryCard({ category, sum, onClick }) {
+  const handleKeyDown = event => {
+    if (event.keyCode === 13 || event.keyCode === 32) {
+      console.log('clicked ✔ ');
+    }
+  };
+
   return (
-    <button onClick={onClick} type="button" className={styles.card}>
-      <p>{sum}</p>
+    // eslint-disable-next-line jsx-a11y/interactive-supports-focus
+    <div
+      className={styles.card}
+      role="button"
+      aria-label={`Отобразить подробный график по категории ${category}`}
+      tabIndex={0}
+      onClick={onClick}
+      onKeyDown={handleKeyDown}
+    >
+      <p className={styles.sum}>{sum}</p>
+
       <CategoryIcon category={category} />
-      <p>{category}</p>
-    </button>
+
+      {category === expense.SPORT ? (
+        <p className={styles.category} style={{ width: '84px' }}>
+          {category}
+        </p>
+      ) : (
+        <p className={styles.category}>{category}</p>
+      )}
+    </div>
   );
 }
 
