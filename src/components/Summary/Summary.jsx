c74d18e3a1f64a01104530360fb8dcc955087a32
IiIymik <sambotemp@gmail.com>
2021-11-18 13:52:27 +0200

    add fixed

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,44 +1,42 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import NumberFormat from 'react-number-format';
 import { fetchTransaction } from '../../services/summaryApi';
 
 import styles from './Summary.module.scss';
-import { summaryOperations, summarySelectors } from '../../redux/transactionSummary';
 
 export default function Summary({ summaryData }) {
-  const dispatch = useDispatch();
-
-  const summary = useSelector(summarySelectors.getSummary);
-  const isLoading = useSelector(summarySelectors.getSummaryIsLoading);
+  const [summaryArr, setSummaryArr] = useState(null);
 
   useEffect(() => {
-    dispatch(summaryOperations.getByMonth());
-  }, [dispatch]);
-
+    if (summaryArr) return;
+    fetchTransaction().then(res => setSummaryArr(res));
+    console.log(summaryArr);
+  }, [summaryArr]);
+  console.log(summaryArr);
   return (
     <div className={styles.summaryWrapper}>
       <p className={styles.summaryTitle}>сводка</p>
       <ul className={styles.summaryList}>
-        {summaryData.map(el => (
-          <li className={styles.summaryItem} key={el.key}>
-            <span className={styles.summaryMonth}>{el.month}</span>
-            <span className={styles.summarySum}>
-              <NumberFormat
-                thousandsGroupStyle="thousand"
-                decimalScale={2}
-                type="text"
-                value={el.sum}
-                displayType="text"
-                allowNegative
-                thousandSeparator={' '}
-                fixedDecimalScale
-                allowEmptyFormatting={false}
-              />
-            </span>
-          </li>
-        ))}
+        {summaryArr &&
+          summaryArr.map(el => (
+            <li className={styles.summaryItem} key={el.key}>
+              <span className={styles.summaryMonth}>{el.reportMonth}</span>
+              <span className={styles.summarySum}>
+                <NumberFormat
+                  thousandsGroupStyle="thousand"
+                  decimalScale={2}
+                  type="text"
+                  value={el.totalResultAmount}
+                  displayType="text"
+                  allowNegative
+                  thousandSeparator={' '}
+                  fixedDecimalScale
+                  allowEmptyFormatting={false}
+                />
+              </span>
+            </li>
+          ))}
       </ul>
     </div>
   );
