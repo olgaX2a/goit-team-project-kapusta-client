f02e56b3c81fe83f4a81bb8378e06cb1eb8862ee
olgaX2a <sekretaryova.olga@gmail.com>
2021-11-21 14:15:18 +0200

    🐛 fix: balance styles
    Styles depends on page & display size

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -34,45 +34,28 @@ const Balance = () => {
 
   return (
     // <div className={!isReportPage ? styles.cont : styles.contReport} />
-    <form className={styles.balanceForm} onSubmit={updateBalance}>
+    <form
+      className={!isReportPage ? styles.balanceForm : styles.balanceFormReport}
+      onSubmit={updateBalance}
+    >
       <label htmlFor="balance" className={styles.balanceLabel}>
         Баланс:
         <div className={styles.buttonsGroup}>
-          {balance === 0 ? (
-            <>
-              {notificationOpen && <Notification onClick={removeNotification} />}
-              <input
-                type="number"
-                step="0.01"
-                name="balance"
-                maxLength="10"
-                placeholder="00.00"
-                onChange={removeNotification}
-                className={!isReportPage ? styles.balanceInput : styles.balanceInputReport}
-                autoComplete="off"
-              />
-              <button
-                className={!isReportPage ? styles.balanceButton : styles.report}
-                type="submit"
-                // onClick={onSubmit}
-              >
-                подтвердить
-              </button>
-            </>
-          ) : (
-            <div className={!isReportPage ? styles.balanceButton : styles.reportForm}>
-              <p className={!isReportPage ? styles.balanceInput : styles.balanceInputReport}>
-                {`${balance.toLocaleString('ru')}.00`} UAH
-              </p>
-              <button
-                className={!isReportPage ? styles.balanceButton : styles.report}
-                type="submit"
-                disabled
-              >
-                подтвердить
-              </button>
-            </div>
-          )}
+          {balance === 0 && <Notification onClick={removeNotification} />}
+          <input
+            type="number"
+            step="0.01"
+            name="balance"
+            maxLength="10"
+            placeholder="00.00"
+            value={balance}
+            onChange={removeNotification}
+            className={!isReportPage ? styles.balanceInput : styles.balanceInputReport}
+            autoComplete="off"
+          />
+          <button className={!isReportPage ? styles.balanceButton : styles.report} type="submit">
+            подтвердить
+          </button>
         </div>
       </label>
     </form>
